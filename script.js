53a5e3b23b2691a6306af6a09258047e7711387d
Saumyajeetsinh Vaghela <103195262+Saumyajeet6103@users.noreply.github.com>
2023-11-30 14:38:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// For Smooth Scrolling Of Navigation Links
+const navLinks = document.querySelectorAll('nav ul li a');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const targetId = e.target.getAttribute('href').slice(1);
+        const targetSection = document.getElementById(targetId);
+        
+        if (targetSection) {
+            window.scrollTo({
+                top: targetSection.offsetTop,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
